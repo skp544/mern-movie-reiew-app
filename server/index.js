3420928b49bae3821d18b4671ffa82ca478e1142
skp544 <shubhamkumarprajapati544@gmail.com>
2023-10-22 11:02:56 +0530

    adding limit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 // imports
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 require("dotenv").config();
 
 // routes
@@ -19,6 +20,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 // middlewares
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
 app.use(
