55f6b609a9f04a644ab754ab8605a22c26abdd42
skp544 <shubhamkumarprajapati544@gmail.com>
2023-10-22 10:58:48 +0530

    adding limit

--- server/index.js
+++ server/index.js
@@ -19,7 +19,8 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 // middlewares
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 app.use(
   cors({
     limit: "50mb",
